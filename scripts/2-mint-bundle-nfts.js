15945202f70b7c7271c87b122ab2a29881a6f7d0
mutugiii <mutugimutuma@gmail.com>
2022-02-07 14:08:34 +0300

    Script to create bundle module && script to mint bundle nfts && script to create pack module && Add assets

---
+++ scripts/2-mint-bundle-nfts.js
@@ -0,0 +1,58 @@
+import { readFileSync } from "fs";
+import { sdk } from "./helpers.js";
+
+async function main() {
+  const bundleModuleAddress = '0xe0fb88d565560B6cb1294ec99d5F3087e16Dd7CB';
+  const bundleModule = sdk.getBundleModule(bundleModuleAddress);
+
+  console.log('Creating NFT batch...');
+
+  const created = await bundleModule.createAndMintBatch([
+    {
+      metadata: {
+        name: 'Tesla Model 3',
+        description: 'A pretty fancy car!',
+        image: readFileSync('./assets/tesla-model3.jpg'),
+        properties: {
+          rarity: 'a bit rare',
+          fanciness: 7,
+        }
+      },
+      supply: 50,
+    },
+    {
+      metadata: {
+        name: 'Porsche 911',
+        description: 'A pretty fancy car!',
+        image: readFileSync('./assets/porsche-911.jpg'),
+        properties: {
+          rarity: 'a bit rare',
+          fanciness: 7,
+        }
+      },
+      supply: 50,
+    },
+    {
+      metadata: {
+        name: 'Mclaren P1',
+        description: 'A super fancy car!',
+        image: readFileSync('./assets/mclaren-p1.jpg'),
+        properties: {
+          rarity: 'super rare!',
+          fanciness: 10,
+        }
+      },
+      supply: 10,
+    }
+  ]);
+
+  console.log('NFTs created!');
+  console.log(JSON.stringify(created, null, 2));
+}
+
+try {
+  await main();
+} catch (error) {
+  console.error("Error minting the NFTs", error);
+  process.exit(1);
+}
